719733de5e1932a431da56a05e315729772b8d79
Hemang Seth <hemang.seth@iiitb.ac.in>
2024-10-08 02:32:47 +0530

    added mobile app functionality

---
+++ supply_chain/mobile_app/app/index.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {Link} from 'expo-router';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App </Text>
+      <Link href="/qr" style={{color:'red'}}>qr</Link>
+      <Link href="/nextStep">nexttep</Link>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        },
+})
