00e8f29dcef95d2bec1e1b018d9eb888ebdccccd
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-10-11 12:37:44 +0530

    Update SignupForm.jsx

--- manufacturing_ui/src/pages/SignupForm.jsx
+++ manufacturing_ui/src/pages/SignupForm.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 // import '../styles/auth.css'
 import { CiUser } from "react-icons/ci";
 import { RiLockPasswordLine } from "react-icons/ri";
-import {Login} from "../components/Login.jsx"; 
+import {Login} from "../components/Login.jsx";
 import { Signup } from '../components/Signup.jsx';
 import { FaFacebookF } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
-import boy from '../assets/a boy standing  5dc9d970-8c4f-48ab-a215-e490926e192d.png'
+// import boy from '../assets/a boy standing  5dc9d970-8c4f-48ab-a215-e490926e192d.png'
+import boy from '../assets/Signup_pic.jpg'
 
 
 const SignupForm = () => {
     return (
         <div className="px-4 max-w-7xl mx-auto lg:space-x-20 flex justify-center items-center h-screen">
+          <div className="w-1/2 hidden lg:block">
+            <img className="rounded-2xl" src={boy} alt="" />
+          </div>
           <div className="lg:w-[40%]">
             <Signup />
             <div className="pb-4 text-sm flex items-center justify-between">
@@ -32,10 +36,8 @@ const SignupForm = () => {
               <p>Sign up with Facebook</p>
             </div>
           </div>
-    
-          <div className="w-1/2 hidden lg:block">
-            <img className="rounded-2xl" src={boy} alt="" />
-          </div>
+
+
         </div>
     );
 }
