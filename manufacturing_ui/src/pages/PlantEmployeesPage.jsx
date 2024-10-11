d640a727c0f24eae6a3d66d18128ed30a37388cf
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-10-12 00:23:07 +0530

    Update PlantEmployeesPage.jsx

--- manufacturing_ui/src/pages/PlantEmployeesPage.jsx
+++ manufacturing_ui/src/pages/PlantEmployeesPage.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 
 const PlantEmployeesPage = () => {
-  return (
+return (
     <div className="flex min-h-screen bg-gray-100">
-        <Sidebar />
-        <div className="flex-grow">
-        <div className="p-4">
-          <div className="bg-white shadow-md p-3 rounded-lg">
-            <p className="mt-1">Name: Aything</p>
-            <p>Email: johndoe@example.com</p>
-            <p>Position: Plant Manager</p>
-          </div>
+            <Sidebar />
+            <div className="flex-grow">
+            <div className="p-4">
+                <div className="bg-white shadow-md p-3 rounded-lg border border-orange-500">
+                    <p className="mt-1">Name: Aything</p>
+                    <p>Email: johndoe@example.com</p>
+                    <p>Position: Plant Manager</p>
+                </div>
+            </div>
+            </div>
         </div>
-        </div>
-      </div>
-  )
+)
 }
 
 export default PlantEmployeesPage
