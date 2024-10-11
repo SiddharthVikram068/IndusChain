a58ff0cba1e790ca3d1575d098d9615db8200c4a
Tanish Pathania <120204495+Tanish-pat@users.noreply.github.com>
2024-10-12 00:20:29 +0530

    Update UserDashboard.jsx

--- manufacturing_ui/src/pages/UserDashboard.jsx
+++ manufacturing_ui/src/pages/UserDashboard.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Sidebar_user from '../components/SIdebar_user';
 
 const UserDashboard = () => {
 
-    const username = "John Doe"; // This will be dynamic later
+    const [email, setEmail] = useState('');
+    const [role, setRole] = useState('');
+
+    useEffect(() => {
+      // Retrieve role and email from localStorage
+      const storedRole = localStorage.getItem('role');
+      const storedEmail = localStorage.getItem('email');
+
+      if (storedRole) setRole(storedRole);
+      if (storedEmail) setEmail(storedEmail);
+    }, []);
+
     return (
       <div className="flex min-h-screen bg-gray-100">
         <Sidebar_user />
         <div className="flex-grow">
-        <div className="p-8">
-          <h2 className="text-3xl font-bold text-orange-600 mb-6">Welcome, {username}</h2>
-          <div className="bg-white shadow-md p-6 rounded-lg">
-            <h3 className="text-xl text-orange-600">Profile Information</h3>
-            <p className="mt-4">Name: {username}</p>
-            <p>Email: johndoe@example.com</p>
-            <p>Position: Plant Manager</p>
+          <div className="p-8">
+            <h2 className="text-3xl font-bold text-orange-600 mb-6">Welcome, {email}</h2>
+            <div className="bg-white shadow-md p-6 rounded-lg border-2 border-orange-600">
+              <h3 className="text-xl text-orange-600">Profile Information</h3>
+              <p className="mt-4">Email: {email}</p>
+              <p>Role: {role}</p>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     );
